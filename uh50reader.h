46173661fda4b1238ae85cc20d2bb8b3e7d65cef
Pär Svanström <par@Pars-MacBook-Pro.local>
2021-10-22 01:15:33 +0200

    add readme, fix code for newest ESP version

--- uh50reader.h
+++ uh50reader.h
@@ -1,3 +1,24 @@
+
+//-------------------------------------------------------------------------------------
+// ESPHome Landis+Gyr Ultraheat UH50 (T550) Heat Meter custom sensor
+// Copyright 2020 Pär Svanström
+// 
+// History
+//  0.1.0 2021-02-03:   Initial release
+//
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+// IN THE SOFTWARE.
+//-------------------------------------------------------------------------------------
+
 #include "esphome.h"
 
 #define BUF_SIZE 100
@@ -25,9 +46,8 @@ class ParsedMessage {
     double cumulativeVolume;
 };
 
-class UH50Reader : public Component {
+class UH50Reader : public Component, public UARTDevice {
   const char* DELIMITERS = "(*";
-  UARTDevice uart_in;
   UARTDevice uart_out;
   unsigned long timeLastRun;
   char buffer[BUF_SIZE];
@@ -36,7 +56,7 @@ class UH50Reader : public Component {
     Sensor *cumulativeActiveImport = new Sensor();
     Sensor *cumulativeVolume = new Sensor();
 
-    UH50Reader(UARTComponent *uart_in, UARTComponent *uart_out) : uart_in(uart_in), uart_out(uart_out) { }
+    UH50Reader(UARTComponent *uart_in, UARTComponent *uart_out) : UARTDevice(uart_in), uart_out(uart_out) { }
 
     void setup() override {
       sendDataCmd();
@@ -102,12 +122,12 @@ class UH50Reader : public Component {
       
       // fast forward until we find the STX byte (start-of-text)
       byte b = 0x00;
-      while (uart_in.available() && b != 0x02) {
-        b = uart_in.read();
+      while (available() && b != 0x02) {
+        b = read();
       }
 
-      while (uart_in.available()) {
-        int len = uart_in.readBytesUntil('\n', buffer, BUF_SIZE);
+      while (available()) {
+        int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
 
         if (len > 0) {
           // end character reached
